3d60089d42899ac173005722c568c8654b5aabfc
Kumar Tejash <132328710+Tejash0@users.noreply.github.com>
2026-03-19 02:29:23 +0530

    0021-MERGE-TWO-SORTED-LISTS Stats: Time: 0 ms (100%), Space: 19.4 MB (62.08%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -12,76 +12,42 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode *head = NULL;
-        ListNode *temp = NULL;
+        ListNode head(0);
+        ListNode *temp = &head;
         while(list1 && list2)
         {
             if(list1->val < list2->val)
             {
-                if(!head && !temp)
-                {
-                    head = list1;
-                    temp = list1;
-                    list1 = list1->next;
-                }
-                else
-                {
+                
+
                     temp->next = list1;
                     temp = list1;
                     list1 = list1->next;
-                }
             }
             else if(list1->val > list2->val)
             {
-                if(!head && !temp)
-                {
-                    head = list2;
-                    temp = list2;
-                    list2 = list2->next;
-                }
-                else
-                {
+                
                     temp->next = list2;
                     temp = list2;
                     list2 = list2->next;
-                }
+                
             }
             else
             {
-                if(!head && !temp)
-                {                    
-                    head = list1;
-                    temp = list1;
-                    list1 = list1->next;
-                    temp->next = list2;
-                    temp = list2;
-                    list2 = list2->next; 
-                }
-                else
-                {
+                
                     temp->next = list1;
                     temp = list1;
                     list1 = list1->next;
                     temp->next = list2;
                     temp = list2;
                     list2 = list2->next;
-                }
+                
             }
         }
         if(!list1)
-        {    
-            if(!head)
-                head = list2;
-            else
             temp->next = list2;
-        }
         else if(!list2)
-        {
-            if(!head)
-                head = list1;
-            else
-                temp->next = list1;
-        }
-        return head;
+            temp->next = list1;
+        return head.next;
     }
 };
