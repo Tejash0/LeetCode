e66b6ae95b8df6b7fd1926d0afae0f8ae65406aa
Kumar Tejash <132328710+Tejash0@users.noreply.github.com>
2026-03-20 21:23:36 +0530

    0792-BINARY-SEARCH Stats: Time: 0 ms (100%), Space: 31.4 MB (44.95%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -4,7 +4,7 @@ public:
         int l=0,r=nums.size()-1;
         while(l<=r)
         {
-            int m = l+(r-l) / 2;
+            int m = (l+r) / 2;
             if(nums[m] == target)
                 return m;
             else if(target > nums[m])
