08dc5b1546a3e0fbd04dec31c7b9afdf569bd4ea
Kumar Tejash <132328710+Tejash0@users.noreply.github.com>
2026-07-19 08:37:08 +0530

    0789-KTH-LARGEST-ELEMENT-IN-A-STREAM Stats: Time: 4 ms (83.37%), Space: 33 MB (63.95%) - LeetHub

---
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,17 @@
+    class KthLargest {
+    public:
+        priority_queue<int,vector<int>, greater<int>> min_heap;
+        int x;
+        KthLargest(int k, vector<int>& nums) {
+            x = k;
+            for(int i : nums)
+                min_heap.push(i);
+            while(min_heap.size() > k)
+                min_heap.pop();
+        }
+        int add(int val) {
+            min_heap.push(val);
+            if(min_heap.size()>x)min_heap.pop();
+            return min_heap.top();
+        }
+    };
