caa12d4f905ccaa08df98341d14df3f4bb7a15a3
Kumar Tejash <132328710+Tejash0@users.noreply.github.com>
2026-07-19 13:57:56 +0530

    1014-K-CLOSEST-POINTS-TO-ORIGIN Stats: Time: 412 ms (5%), Space: 61.6 MB (99.09%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,51 +1,30 @@
 class Solution {
 public:
-    int value(vector<int> & a)
-    {
-        return a[0]*a[0] + a[1] * a[1];
+    int dist(const vector<int>& p) {
+        return p[0] * p[0] + p[1] * p[1];
     }
+
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        int i = 0,j = 0;
-        int l = 0,r = points.size()-1;
-        while(l<=r)
-        {
-            int ran = l + rand() % (r - l + 1);
-            swap(points[ran],points[r]);
-            int p = r;
-            
-            while(j<=p)
-            {
-                if(j == p)
-            {
-                swap(points[i],points[p]);
-                if(k == i)return vector<vector<int>>(points.begin(),points.begin()+k);
-                else if(k < i)
-                {
-                    r = i-1;
-                    i = l;
-                    j = l;
-                }
-                else
-                {
-                    i = i+1;
-                    l = i;
-                    j = i;
-                }
-                break;  
-            }
-            else if(value(points[j]) < value(points[p]))
-            {
-                swap(points[i],points[j]);
-                i++;
-                j++;
-            }
-            else
-            {
-                j++;
-            }
+        int left = 0, right = points.size() - 1;
+        while (left <= right) {
+            int pivotIndex = partition(points, left, right);
+            if (pivotIndex == k) break;
+            if (pivotIndex < k) left = pivotIndex + 1;
+            else right = pivotIndex - 1;
+        }
+        return vector<vector<int>>(points.begin(), points.begin() + k);
+    }
+
+private:
+    int partition(vector<vector<int>>& points, int left, int right) {
+        int pivot = dist(points[right]);
+        int i = left;
+        for (int j = left; j < right; ++j) {
+            if (dist(points[j]) < pivot) {
+                swap(points[i++], points[j]);
             }
         }
-
-        return vector<vector<int>>(points.begin(),points.begin()+k);
+        swap(points[i], points[right]);
+        return i;
     }
 };
