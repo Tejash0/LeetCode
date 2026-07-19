39a5f65b177bf042d60b01716847b89eeaa4877e
Kumar Tejash <132328710+Tejash0@users.noreply.github.com>
2026-07-19 13:59:06 +0530

    1014-K-CLOSEST-POINTS-TO-ORIGIN Stats: Time: 14 ms (97.81%), Space: 61.8 MB (94.28%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,30 +1,14 @@
 class Solution {
 public:
-    int dist(const vector<int>& p) {
-        return p[0] * p[0] + p[1] * p[1];
+    static bool comp(vector<int> & a,vector<int>& b)
+    {
+        int d1 = a[0]*a[0] + a[1] * a[1];
+        int d2 = b[0]*b[0] + b[1] * b[1];
+        return d1 < d2;
     }
-
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        int left = 0, right = points.size() - 1;
-        while (left <= right) {
-            int pivotIndex = partition(points, left, right);
-            if (pivotIndex == k) break;
-            if (pivotIndex < k) left = pivotIndex + 1;
-            else right = pivotIndex - 1;
-        }
-        return vector<vector<int>>(points.begin(), points.begin() + k);
-    }
+        nth_element(points.begin(),points.begin()+k,points.end(),comp);
 
-private:
-    int partition(vector<vector<int>>& points, int left, int right) {
-        int pivot = dist(points[right]);
-        int i = left;
-        for (int j = left; j < right; ++j) {
-            if (dist(points[j]) < pivot) {
-                swap(points[i++], points[j]);
-            }
-        }
-        swap(points[i], points[right]);
-        return i;
+        return vector<vector<int>>(points.begin(),points.begin()+k);
     }
 };
